caeb424da6d24bb2c0f26af5f77b7ef3556be274
Rahul Tripathi <rahultripathidev@gmail.com>
2024-05-24 02:32:38 +0530

    ft: add address consts

---
+++ types/types.go
@@ -0,0 +1,9 @@
+package types
+
+import "github.com/ethereum/go-ethereum/common"
+
+var (
+	Address0x   = common.HexToAddress("0x0000000000000000000000000000000000000000")
+	Address0x1  = common.HexToAddress("0x0000000000000000000000000000000000000001")
+	Address0x69 = common.HexToAddress("0x0000000000000000000000000000000000000069")
+)
