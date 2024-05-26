9eebbe9520a940c25b4016091c1e3819c209041c
Rahul Tripathi <rahultripathidev@gmail.com>
2024-05-26 16:26:17 +0530

    ft: add fork config

---
+++ statedb/interface.go
@@ -0,0 +1,14 @@
+package statedb
+
+import (
+	"context"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/rahul0tripathi/smelter/entity"
+)
+
+type forkDB interface {
+	State(ctx context.Context, addr common.Address) (*entity.AccountState, *entity.AccountStorage, error)
+	CreateState(ctx context.Context, addr common.Address) error
+	GetState(ctx context.Context, addr common.Address, hash common.Hash) (common.Hash, error)
+}
