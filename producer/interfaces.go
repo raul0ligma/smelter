ff12b11c672c8138ddcaf3949c086c82c87cc41d
Rahul Tripathi <rahultripathidev@gmail.com>
2024-06-01 00:31:23 +0530

    rm: add block producers

---
+++ producer/interfaces.go
@@ -0,0 +1,19 @@
+package producer
+
+import (
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/rahul0tripathi/smelter/entity"
+)
+
+type transactionStorage interface {
+	AddTransaction(tx *types.Transaction)
+	AddReceipt(receipt *types.Receipt)
+}
+
+type postExecutionStateFetcher interface {
+	Logs() entity.LogStorage
+}
+
+type blockStorage interface {
+	AddBlock(block *types.Block)
+}
