c0fa7f8dbcb0d9b1c90c6be074121acef44e3f0f
Rahul Tripathi <rahultripathidev@gmail.com>
2024-06-17 04:32:19 +0530

    ft: derive caller key from ctx

--- pkg/server/mw.go
+++ pkg/server/mw.go
@@ -9,9 +9,11 @@ import (
 
 const (
 	_headerCaller = "X-Caller"
+	_paramKey     = "key"
 )
 
 type Caller struct{}
+type Key struct{}
 
 func SetResponseHeaderMw(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
@@ -25,6 +27,18 @@ func SetCallerContextMw(next echo.HandlerFunc) echo.HandlerFunc {
 		caller := c.Request().Header.Get(_headerCaller)
 		if common.IsHexAddress(caller) {
 			ctx := context.WithValue(c.Request().Context(), Caller{}, common.HexToAddress(caller))
+			c.SetRequest(c.Request().WithContext(ctx))
+		}
+
+		return next(c)
+	}
+}
+
+func SetExecutionContextMw(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		caller := c.Param(_paramKey)
+		if caller != "" {
+			ctx := context.WithValue(c.Request().Context(), Key{}, caller)
 			c.SetRequest(c.Request().WithContext(ctx))
 		}
 
