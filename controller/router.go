e37f052e73a573de9e6fab94c09d1589e0816468
Rahul Tripathi <rahultripathidev@gmail.com>
2024-06-17 17:31:37 +0530

    ft: add cors handler

--- controller/router.go
+++ controller/router.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/rahul0tripathi/go-jsonrpc"
 	"github.com/rahul0tripathi/smelter/pkg/log"
 	"github.com/rahul0tripathi/smelter/pkg/server"
@@ -12,5 +13,10 @@ func SetupRouter(
 	rpcServer *jsonrpc.RPCServer,
 	logger log.Logger,
 ) {
-	router.POST("/v1/rpc/:key", echo.WrapHandler(rpcServer), server.SetExecutionContextMw, server.SetCallerContextMw, server.SetResponseHeaderMw)
+	router.POST("/v1/rpc/:key", echo.WrapHandler(rpcServer),
+		middleware.CORSWithConfig(middleware.DefaultCORSConfig),
+		server.SetExecutionContextMw,
+		server.SetCallerContextMw,
+		server.SetResponseHeaderMw,
+	)
 }
