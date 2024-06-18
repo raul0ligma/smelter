b02bc53f4b60ae27b0d385e74c9ffd3874e39302
Rahul Tripathi <rahultripathidev@gmail.com>
2024-06-18 18:38:53 +0530

    ft: update utils and types

--- utils/convertors.go
+++ utils/convertors.go
@@ -13,3 +13,15 @@ func Big2Hex(v *big.Int) string {
 
 	return hexutil.Encode(v.Bytes())
 }
+
+func ToEvenLength(hex string) string {
+	if len(hex) < 2 || hex[:2] != "0x" {
+		return hex
+	}
+
+	if len(hex[2:])%2 != 0 {
+		return "0x0" + hex[2:]
+	}
+
+	return hex
+}
